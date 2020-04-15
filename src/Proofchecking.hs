2a759dc438bd1e8a8541227f8f796ba694ea40d8
peng fu <fermat1217@gmail.com>
2020-04-15 15:37:01 -0300

    minor bug fix due to stack not compiling.

--- src/Proofchecking.hs
+++ src/Proofchecking.hs
@@ -568,7 +568,7 @@ proofCheck flag a@(Case tm (B brs)) goal =
      let Just (Left id, _) = t'
      id' <- lookupId id
      case identification id' of
-          DataType Simple _ _ -> throwError (DataErr at tm)
+          DataType Simple _ _ -> throwError (DataErr t tm)
           DataType _ _ _ -> return ()         
      updateCountWith (\ x -> enterCase x id)
      checkBrs t brs goal
