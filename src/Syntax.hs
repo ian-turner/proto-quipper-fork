d9dc7eb9b53f937e9b9b1089ac9a85f5f137c556
peng fu <fermat1217@gmail.com>
2020-04-17 10:40:05 -0300

    make gate displaying control flag

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -554,10 +554,10 @@ instance Disp Morphism where
    (braces $ display flag outs) 
 
 instance Disp Gate where
-  display flag (Gate g params ins outs ctrls _) =
+  display flag (Gate g params ins outs ctrls b) =
     display flag g <+> brackets (hsep $ punctuate comma (map (display flag) params))
     <+> (braces $ (display flag ins)) <+> (braces $ (display flag outs))
-    <+> (display flag ctrls)
+    <+> (display flag ctrls) <+> text (show b)
 
 -- | Convert a /basic value/ from the value domain to an expression,
 -- so that the type checker can take advantage of cbv.   
