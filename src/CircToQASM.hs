0066c4a0f86d86e0687f26c771f1d189f633cf54
ian-turner <i.r.turner55@gmail.com>
2025-06-26 19:46:17 -0400

    added qubit mapping

--- src/CircToQASM.hs
+++ src/CircToQASM.hs
@@ -35,29 +35,39 @@ to_qasm_gate "T*" = "tdg"
 to_qasm_gate "CNot" = "cx"
 to_qasm_gate "Rot" = "rz"
 
+qasm_header = "OPENQASM 3.0;\ninclude \"qelib1.inc\";"
+
+
+-- Query map object for qubit label
+label_to_qubit m l =
+    case Map.lookup l m of
+        Nothing -> error "QASM converter error"
+        Just y -> "qubits[" ++ (show y) ++ "]"
+        
 
 -- Converts gate to QASM format
 
 -- Init gates
-gate_to_qasm (Gate (Id gateName) _ (VStar) (VLabel l) _ _ _ _ _)
+gate_to_qasm qubit_map (Gate (Id gateName) _ (VStar) (VLabel l) _ _ _ _ _)
     | gateName == "Init0" =
         "qubit " ++ (show l) ++ ";\nreset " ++ (show l) ++ ";"
 
-gate_to_qasm (Gate (Id gateName) _ (VStar) (VLabel l) _ _ _ _ _)
+gate_to_qasm qubit_map (Gate (Id gateName) _ (VStar) (VLabel l) _ _ _ _ _)
     | gateName == "Init1" =
         "qubit " ++ (show l) ++ ";\nreset " ++ (show l) ++ ";\nx " ++ (show l) ++ ";"
 
 -- Single qubit gates
-gate_to_qasm (Gate (Id gateName) _ (VLabel l) output ctrl _ _ _ _)
+gate_to_qasm qubit_map (Gate (Id gateName) _ (VLabel l) output ctrl _ _ _ _)
     | (gateName == "Meas" || gateName == "Discard") =
         "bit b_" ++ (show l) ++ ";\nb_" ++ (show l) ++ " = measure " ++ (show l) ++ ";"
     
-gate_to_qasm (Gate (Id gateName) _ (VLabel l) output ctrl _ _ _ _) =
-    (to_qasm_gate gateName) ++ " " ++ (show l) ++ ";"
+gate_to_qasm qubit_map (Gate (Id gateName) _ (VLabel l) output ctrl _ _ _ _) =
+    (to_qasm_gate gateName) ++ " " ++ (label_to_qubit qubit_map l) ++ ";"
 
 -- Two qubit gates
-gate_to_qasm (Gate (Id gateName) _ (VPair (VLabel l1) (VLabel l2)) output ctrl _ _ _ _) =
-    (to_qasm_gate gateName) ++ " " ++ (show l2) ++ ", " ++ (show l1) ++ ";"
+gate_to_qasm qubit_map (Gate (Id gateName) _ (VPair (VLabel l1) (VLabel l2)) output ctrl _ _ _ _) =
+    (to_qasm_gate gateName) ++ " " ++ (label_to_qubit qubit_map l2) ++ ", "
+        ++ (label_to_qubit qubit_map l1) ++ ";"
 
 
 string_join :: String -> [String] -> String
@@ -70,12 +80,19 @@ string_join s (x:xs) = x ++ s ++ (string_join s xs)
 circ_to_qasm circ =
     open circ $ \ _ morph ->
         let q1 = input morph
+            -- Parsing circuit object to extract gates
             ocirc = gates morph
             (gs, _) = refresh_gates Map.empty ocirc []
             ws = getWires q1 `List.union` wirelist gs
             num_qubits = fromIntegral $ List.length ws
-            gates_qasm = map gate_to_qasm gs
-        in (string_join "\n" ("OPENQASM 3.0;" : "include \"qelib1.inc\";" : gates_qasm))
+            qubit_map = Map.fromList $ zip ws [0..(num_qubits-1)]
+
+            -- Mapping over gates to convert each to qasm
+            gates_qasm = map (gate_to_qasm qubit_map) gs
+            qreg_init = "qreg qubits[" ++ (show num_qubits) ++ "];"
+
+        -- Joining all gate strings together with header
+        in (string_join "\n" (qasm_header : qreg_init : gates_qasm))
 
 
 -- Runs the OpenQASM converter and stores result to text file
