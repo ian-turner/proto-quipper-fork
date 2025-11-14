39355e48406b27c82d69314b089be60d42bb6e63
ian-turner <i.r.turner55@gmail.com>
2025-11-14 09:47:14 -0500

    Changed type of qasm loader function

--- src/QasmLoader.hs
+++ src/QasmLoader.hs
@@ -3,8 +3,12 @@ module QasmLoader where
 import System.IO
 import QasmParser
 
+import Syntax
 
+
+loadQasmFile :: String -> Top String
 loadQasmFile file = do
     (Program _ stmts) <- parseQasmFile file
     let stmts_str = map show stmts
-    putStr (unlines stmts_str)
+    ioTop $ putStr (unlines stmts_str)
+    return "test"
